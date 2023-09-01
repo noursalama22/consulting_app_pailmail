835d40500661afe1e35e20c36f0566a99e9617f7
Nour Salama <salama.nour.ta@gmail.com>
2023-09-02 00:28:24 +0300

    Initial commit

---
+++ android/app/src/main/java/com/example/consulting_app_pailmail/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.consulting_app_pailmail;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/helpers/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ============= COLORS CONSTS ==============
+const Color kLightBlueColor = Color(0xFF6589FF);
+const Color kPrimaryBlueColor = Color(0xFF003AFC);
+const Color kDarkGreyColor = Color(0xFF7C7C7C);
+const Color kGreenColor = Color(0xFF77D16F);
+const Color kYellowColor = Color(0xFFFFE120);
+const Color kRedColor = Color(0xFFFA3A57);
+const Color kBlackColor = Color(0xFF272727);
+const Color kLightGreyColor = Color(0xFFE6E6E6);
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kMediumGreyColor = Color(0xFFB2B2B2);
+const Color kBackgroundColor = Color(0xFFF7F6FF);
+
+// ============= TEXT STYLES CONSTS ==============
